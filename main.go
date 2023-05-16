7af04437c5317ecbc285918beeea115cc9806d08
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-05-17 00:10:58 +0900

    todo関係のインスタンスを作成

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"go-rest-api/usecase"
 )
 
-func main( ) {
+func main() {
 	db := db.NewDB()
 	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
