0f736c16432a71511849623e2e6a19b333ad310b
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-05-20 17:51:57 +0900

    csrfを確認するミドルウェアを追加してcorsを設定

--- router/router.go
+++ router/router.go
@@ -2,20 +2,43 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"net/http"
 	"os"
 
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		// header一覧とecho.HeaderXCSRFTokenでheaderでcsrf tokenを受け取れるようにしている
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+		AllowMethods: []string{"GET", "PUT", "POST", "DELETE"},
+		// cookieの送受信を可能に
+		AllowCredentials: true,
+	}))
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		CookiePath:     "/",
+		CookieDomain:   os.Getenv("API_DOMAIN"),
+		CookieHTTPOnly: true,
+		// 本番用
+		CookieSameSite: http.SameSiteNoneMode,
+
+		// テスト用
+		// CookieSameSite: http.SameSiteDefaultMode,
+		//CookieMaxAge:   60,
+	}))
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	e.GET("/csrf", uc.CsrfToken)
 	t := e.Group("/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
-		SigningKey:  []byte(os.Getenv("SECRET")),
+		SigningKey: []byte(os.Getenv("SECRET")),
 		// どこに保存してあるか
 		TokenLookup: "cookie:token",
 	}))
