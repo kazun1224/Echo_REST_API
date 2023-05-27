2ccf0e35440012f1c4d746c08b15a7a79ae0daba
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-05-27 11:06:24 +0900

    サブスクリプション用のモデルを追加

--- model/user.go
+++ model/user.go
@@ -3,11 +3,14 @@ package model
 import "time"
 
 type User struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Email     string    `json:"email" gorm:"unique"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	Email          string    `json:"email" gorm:"unique"`
+	Password       string    `json:"password"`
+	LineId         string    `json:"line_id"`
+	IsSubscribed   string    `json:"is_subscribed"`
+	StripeCustomer string    `json:"stripe_customer"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 type UserResponse struct {
